9468a5b83298d4fe136cd8f243929c1c1108d6dc
idrissou02 <idriss.souhil@gmail.com>
2024-06-06 16:58:24 +0200

    2eme commit

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; 2024 Votre Entreprise</p>
+</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Présentation</title>
+    <link rel="stylesheet" href="/ressources/style.css">
+</head>
+<header>
+    <h1>Bienvenue sur notre site</h1>
+    <nav>
+        <a href="/index.php">Accueil</a>
+        <a href="/pages/presentation.php">Présentation</a>
+        <a href="/pages/catalog.php">Catalogue</a>
+        <a href="/pages/contact.php">Contact</a>
+    </nav>
+</header>
